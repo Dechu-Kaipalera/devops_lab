111e550a6aec957b49a25d5b7f072548511acf19
Dechu-Kaipalera <156814531+Dechu-Kaipalera@users.noreply.github.com>
2024-12-09 10:04:58 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,2 +1,6 @@
-System.out.println("Hello,World!");
-System.out.println("vvce");
+public class Sample {
+  public static void main(String[] args) {
+    System.out.println("Hello,World!");
+    System.out.println("vvce");
+  }
+}
